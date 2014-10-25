aad97642acd16305b9e964ebdfcb810598705033
Tobias Lundin <tobias.lundin@gmail.com>
2014-10-25 15:55:17 +0200

    initial commit

---
+++ ItsPersonal/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItsPersonal.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("about")]
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		[Route("slides")]
+		public ActionResult Slides()
+		{
+			ViewBag.Message = "Slides.";
+
+			return View();
+		}
+
+		[Route("contact")]
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ ItsPersonal/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ItsPersonal.Extensions
+{
+	public static class UrlHelperExtensions
+	{
+		public static string Image(this UrlHelper url, string imagePath)
+		{
+			return url.Content("~/content/images/" + imagePath);
+		}
+
+		public static string Html(this UrlHelper url, string htmlPath)
+		{
+			return url.Content("~/content/html/" + htmlPath);
+		}
+	}
+}
