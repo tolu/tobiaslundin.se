68866052e5a3231c1120b043612d08c3d9b7d967
Tobias Lundin <tobias.lundin@gmail.com>
2016-04-09 18:29:28 +0200

    update to newer version of dotnet core. remove unneccessary stuff

--- src/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="lib/jquery/jquery.js" />
-/// <reference path="lib/jquery/jquery-migrate.js" />
-/// <reference path="lib/hammer.js/hammer.js" />
-/// <reference path="lib/bootstrap/js/bootstrap.js" />
